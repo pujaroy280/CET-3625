4ca35ef35a0203ba2aae92e3527f03171dfc448e
Puja Roy <62675121+pujaroy280@users.noreply.github.com>
2022-06-02 17:22:26 -0400

    Create midterm.m

---
+++ midterm.m
@@ -0,0 +1,18 @@
+nthroot(27,3) %a)
+nthroot(1,5)  %b)
+nthroot(-1,3) %c)
+
+
+clc
+clear
+%4
+% a
+disp(['Answer to 4a is: ',num2str(1i^2)]);%i is represented by 1i
+% b
+disp(['Answer to 4b is: ',num2str( log((1+1i)/2))]);
+% c
+disp(['Answer to 4c is: ',num2str((1-1i)^(1i+3))]);
+% d
+disp(['Answer to 4d is: ',num2str( sin(log((1-1i)/(1+1i))))]);
+
+
